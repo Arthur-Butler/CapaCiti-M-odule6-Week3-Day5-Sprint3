29a83abf45aa0d9ce457c5c77920b8f4ba85872e
CPTWEX012 <62343020+CPTWEX012@users.noreply.github.com>
2020-07-24 15:56:36 +0200

    Update Database_Interpreter.py

--- Database_Interpreter.py
+++ Database_Interpreter.py
@@ -43,153 +43,143 @@ plt.show()
 mycursor.execute("SELECT `chips`.`Stock Amount` FROM chips")
 ItemAmount=mycursor.fetchall()
 #Placing data in lists
-y1=[]
+sizes1=[]
 for row in ItemAmount:
-    y1.extend(row)
-print(y1)
+    sizes1.extend(row)
+print(sizes1)
 mycursor.execute("SELECT `chips`.`Brand_Name` FROM chips")
 Items=mycursor.fetchall()
-x1=[]
+labels1=[]
 for row in Items:
-    x1.extend(row)
-print(x1)    
+    labels1.extend(row)
+print(labels1)    
 y_pos=np.arange(len(x1))
-#Diplaying data viually using a bar graph
-plt.bar(y_pos, y1, align='center', alpha=1)
-plt.xticks(y_pos, x1)
-plt.ylabel('Stock Amount')
-plt.xlabel('Brand')
-plt.title('Amount of stock for each item in a category')
+#Diplaying data viually using a piechart
+colors=['red','blue','green','yellow','purple','pink','orange']
+explode=(0,0,0,0,0,0,0)
+plt.pie(sizes1, explode=explode, labels=labels1, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
 plt.show()
 
 mycursor.execute("SELECT cooldrinks.Stock_Amount FROM cooldrinks")
 ItemAmount=mycursor.fetchall()
 #Placing data in lists
-y2=[]
+sizes2=[]
 for row in ItemAmount:
-    y2.extend(row)
-print(y2)
+    sizes2.extend(row)
+print(sizes2)
 mycursor.execute("SELECT Brand_Name FROM cooldrinks")
 Items=mycursor.fetchall()
-x2=[]
+labels2=[]
 for row in Items:
-    x2.extend(row)
-print(x2)    
+    labels2.extend(row)
+print(labels2)    
 y_pos=np.arange(len(x2))
-#Diplaying data viually using a bar graph
-plt.bar(y_pos, y2, align='center', alpha=1)
-plt.xticks(y_pos, x2)
-plt.ylabel('Stock Amount')
-plt.xlabel('Brand')
-plt.title('Amount of stock for each item in a category')
+#Diplaying data viually using a piechart
+colors=['red','blue','green','yellow','purple','pink','orange']
+explode=(0,0,0,0,0,0,0)
+plt.pie(sizes2, explode=explode, labels=labels2, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
 plt.show()
 
 mycursor.execute("SELECT chocolates.Stock_Amount FROM chocolates")
 ItemAmount=mycursor.fetchall()
 #Placing data in lists
-y3=[]
+sizes3=[]
 for row in ItemAmount:
-    y3.extend(row)
-print(y3)
+    sizes3.extend(row)
+print(sizes3)
 mycursor.execute("SELECT Brand_Name FROM chocolates")
 Items=mycursor.fetchall()
-x3=[]
+labels3=[]
 for row in Items:
-    x3.extend(row)
-print(x3)    
-y_pos=np.arange(len(x3))
-#Diplaying data viually using a bar graph
-plt.bar(y_pos, y3, align='center', alpha=1)
-plt.xticks(y_pos, x3)
-plt.ylabel('Stock Amount')
-plt.xlabel('Brand')
-plt.title('Amount of stock for each item in a category')
+    labels3.extend(row)
+print(labels3)    
+#Diplaying data viually using a piechart
+colors=['red','blue','green','yellow','purple','pink','orange','white']
+explode=(0,0,0,0,0,0,0,0)
+plt.pie(sizes3, explode=explode, labels=labels3, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
 plt.show()
+
 
 mycursor.execute("SELECT cupcakes.Stock_Amount FROM cupcakes")
 ItemAmount=mycursor.fetchall()
 #Placing data in lists
-y4=[]
+sizes4=[]
 for row in ItemAmount:
-    y4.extend(row)
-print(y4)
+    sizes4.extend(row)
+print(sizes4)
 mycursor.execute("SELECT Brand_Name FROM cupcakes")
 Items=mycursor.fetchall()
-x4=[]
+labels4=[]
 for row in Items:
-    x4.extend(row)
-print(x4)    
-y_pos=np.arange(len(x4))
-#Diplaying data viually using a bar graph
-plt.bar(y_pos, y4, align='center', alpha=1)
-plt.xticks(y_pos, x4)
-plt.ylabel('Stock Amount')
-plt.xlabel('Brand')
-plt.title('Amount of stock for each item in a category')
+    labels4.extend(row)
+print(labels4)    
+#Diplaying data viually using a piechart
+pcolors=['red','blue','green','yellow']
+explode=(0,0,0,0)
+plt.pie(sizes4, explode=explode, labels=labels4, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
 plt.show()
 
 mycursor.execute("SELECT fruit.Stock_Amount FROM fruit")
 ItemAmount=mycursor.fetchall()
 #Placing data in lists
-y5=[]
+sizes5=[]
 for row in ItemAmount:
-    y5.extend(row)
-print(y5)
+    sizes5.extend(row)
+print(sizes5)
 mycursor.execute("SELECT Brand_Name FROM fruit")
 Items=mycursor.fetchall()
-x5=[]
+labels5=[]
 for row in Items:
-    x5.extend(row)
-print(x5)    
-y_pos=np.arange(len(x5))
-#Diplaying data viually using a bar graph
-plt.bar(y_pos, y5, align='center', alpha=1)
-plt.xticks(y_pos, x5)
-plt.ylabel('Stock Amount')
-plt.xlabel('Brand')
-plt.title('Amount of stock for each item in a category')
+    labels5.extend(row)
+print(labels5)    
+#Diplaying data viually using a piechart
+colors=['red','blue','green','yellow','purple','pink','orange','white']
+explode=(0,0,0,0,0,0,0,0)
+plt.pie(sizes5, explode=explode, labels=labels5, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
 plt.show()
 
 mycursor.execute("SELECT pies.Stock_Amount FROM pies")
 ItemAmount=mycursor.fetchall()
 #Placing data in lists
-y6=[]
+sizes6=[]
 for row in ItemAmount:
-    y6.extend(row)
-print(y6)
+    sizes6.extend(row)
+print(sizes6)
 mycursor.execute("SELECT Brand_Name FROM pies")
 Items=mycursor.fetchall()
-x6=[]
+labels6=[]
 for row in Items:
-    x6.extend(row)
-print(x6)    
-y_pos=np.arange(len(x6))
-#Diplaying data viually using a bar graph
-plt.bar(y_pos, y6, align='center', alpha=1)
-plt.xticks(y_pos, x6)
-plt.ylabel('Stock Amount')
-plt.xlabel('Brand')
-plt.title('Amount of stock for each item in a category')
+    labels6.extend(row)
+print(labels6)    
+#Diplaying data viually using a piechart
+colors=['red','blue','green','yellow','purple','pink','orange']
+explode=(0,0,0,0,0,0,0)
+plt.pie(sizes6, explode=explode, labels=labels6, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
 plt.show()
 
 mycursor.execute("SELECT veggies.Stock_Amount FROM veggies")
 ItemAmount=mycursor.fetchall()
 #Placing data in lists
-y7=[]
+sizes7=[]
 for row in ItemAmount:
-    y7.extend(row)
-print(y7)
+    sizes7.extend(row)
+print(sizes7)
 mycursor.execute("SELECT Brand_Name FROM veggies")
 Items=mycursor.fetchall()
-x7=[]
+labels7=[]
 for row in Items:
-    x7.extend(row)
-print(x7)    
-y_pos=np.arange(len(x7))
-#Diplaying data viually using a bar graph
-plt.bar(y_pos, y7, align='center', alpha=1)
-plt.xticks(y_pos, x7)
-plt.ylabel('Stock Amount')
-plt.xlabel('Brand')
-plt.title('Amount of stock for each item in a category')
+    labels7.extend(row)
+print(labels7)    
+#Diplaying data viually using a piechart
+colors=['red','blue','green','yellow','purple']
+explode=(0,0,0,0,0)
+plt.pie(sizes7, explode=explode, labels=labels7, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
 plt.show()
